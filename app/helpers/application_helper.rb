ca9ea236fea31382ae2c794c9734e8cd6758e914
NiushaH <Niusha.Hadziomerovic@gmail.com>
2020-07-27 23:52:22 -0600

    update to use logged_in? across app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
   def current_user
     if session[:current_user_id]
+    # Memoization = cheap caching so that database isn't called every time a web request is sent 
       @current_user ||= User.find(session[:current_user_id])
     end
   end  
